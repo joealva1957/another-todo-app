1145e8cc255ac250d7795805f43fc286b1d5ee42
joelava1957 <joelava1957@gmail.com>
2023-10-10 00:21:48 -0600

    A new vranch with some changes to main

--- main.js
+++ main.js
@@ -41,3 +41,13 @@ let acceptData = () => {
 
   console.log(data);
 };
+
+let deleteTask = (e) => {
+    e.parentElement.parentElement.remove();
+  
+    data.splice(e.parentElement.parentElement.id, 1);
+  
+    localStorage.setItem("data", JSON.stringify(data));
+  
+    console.log(data);
+  };
