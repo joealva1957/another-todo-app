3f83b78726ccc5182509b78999217382b5e9d154
joelava1957 <joelava1957@gmail.com>
2023-10-23 17:19:07 -0600

    Add Edit function to to-do app

--- main.js
+++ main.js
@@ -77,3 +77,19 @@ let deleteTask = (e) => {
 
   console.log(data);
 };
+
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  console.log(data);
+  createTasks();
+})();
